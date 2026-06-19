45ba59f7bb6eb95def68054ceeacf05e1b046cdc
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-06-19 06:16:51 +0100

    About me update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,6 +7,11 @@ function About() {
         the NaijaJUG community and proud to be part of the first cohort of the
         NaijaJUG Software Development Bootcamp.
       </p>
+      <p>
+        I enjoy learning and understanding how systems that run the backbone of
+        a business are built and serve millions of users without performace drop
+        (it is just fascinating to me😅).
+      </p>
     </section>
   );
 }
